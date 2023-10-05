c9f86ec299c4db454b0394169a70fce3ad36372c
[Niki Lalev] <[nikcho27@gmail.com]>
2023-10-05 17:02:34 +0200

    bug_fix

--- reader.py
+++ reader.py
@@ -1,17 +1,22 @@
 import sqlite3
+import table_clear
 
 # Define the SQLite database file
 db_file = "game.db"
 
 # Open a connection to the database and create a cursor
+#table_clear.table_clean(db_file)
 conn = sqlite3.connect(db_file)
 cursor = conn.cursor()
 
 # Function to parse and load data for a table
 def load_table(table_name, data):
-    insert_sql = f"INSERT INTO {table_name} ({', '.join(data.keys())}) VALUES ({', '.join(data.values())})"
-    insert_sql.replace('"', "'");
+    values = [str(value).replace('"', "'") for value in data.values()]
+    values = [str(value).replace("'", "\'") for value in data.values()]
+    insert_sql = f"INSERT INTO {table_name} ({', '.join(data.keys())}) VALUES ({', '.join(values)})"
+    
     cursor.execute(insert_sql)
+    conn.commit()
 
 # Read and process the text file
 with open("generated_entities.txt", "r") as file:
--- table_clear.py
+++ table_clear.py
@@ -1,19 +1,18 @@
 import sqlite3
 
-# Define the SQLite database file
-db_file = "game.db"
+def table_clean(db_file):
 
-# Open a connection to the database and create a cursor
-conn = sqlite3.connect(db_file)
-cursor = conn.cursor()
+    # Open a connection to the database and create a cursor
+    conn = sqlite3.connect(db_file)
+    cursor = conn.cursor()
 
-# Specify the table names you want to remove all entries from
-table_names = ["event", "item", "vendor", "team", "question", "npc", "character", "guild", "enemy"]
+    # Specify the table names you want to remove all entries from
+    table_names = ["event", "item", "vendor", "team", "question", "npc", "character", "guild", "enemy"]
 
-# Iterate through the list of table names and delete all entries from each table
-for table_name in table_names:
-    cursor.execute(f"DELETE FROM {table_name}")
+    # Iterate through the list of table names and delete all entries from each table
+    for table_name in table_names:
+        cursor.execute(f"DELETE FROM {table_name}")
 
-# Commit the changes and close the database connection
-conn.commit()
-conn.close()
+    # Commit the changes and close the database connection
+    conn.commit()
+    conn.close()
